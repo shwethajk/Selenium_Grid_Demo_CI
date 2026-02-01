2f46a92796f1868cd9ebac6d62c6a830a511eb99
Shwetha Kulal <90205229+shwethajk@users.noreply.github.com>
2026-02-01 23:32:51 +0530

    Refactor DriverFactory to use environment variables

--- src/test/java/base/DriverFactory.java
+++ src/test/java/base/DriverFactory.java
@@ -1,3 +1,11 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
 public class DriverFactory {
 
     public static WebDriver getDriver(String browser) throws Exception {
@@ -9,13 +17,11 @@ public class DriverFactory {
         if (browser.equalsIgnoreCase("chrome")) {
             gridUrl = System.getenv().getOrDefault(
                     "CHROME_GRID_URL",
-                    "http://localhost:4444"
-            );
+                    "http://localhost:4444");
         } else if (browser.equalsIgnoreCase("firefox")) {
             gridUrl = System.getenv().getOrDefault(
                     "FIREFOX_GRID_URL",
-                    "http://localhost:4445"
-            );
+                    "http://localhost:4445");
         } else {
             throw new RuntimeException("Unsupported browser: " + browser);
         }
@@ -23,3 +29,28 @@ public class DriverFactory {
         return new RemoteWebDriver(new URL(gridUrl), caps);
     }
 }
+
+// package base;
+
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.remote.RemoteWebDriver;
+// import org.openqa.selenium.remote.DesiredCapabilities;
+
+// import java.net.URL;
+
+// public class DriverFactory {
+
+// public static WebDriver getDriver(String browser) throws Exception {
+// DesiredCapabilities caps = new DesiredCapabilities();
+// caps.setBrowserName(browser);
+// // local standalone grid
+// // return new RemoteWebDriver(new URL("http://localhost:4444"), caps);
+
+// // Use for both browsers (2 standalone grids)
+// // select grid url from environment variable or use default
+// String gridUrl = System.getenv().getOrDefault(
+// "GRID_URL",
+// "http://localhost:4444");
+// return new RemoteWebDriver(new URL(gridUrl), caps);
+// }
+// }
