def5f1637332e4b60091fef6dbf5d797cf39972b
Shwetha Kulal <90205229+shwethajk@users.noreply.github.com>
2026-02-01 23:14:59 +0530

    Update getDriver method to use environment variable
    Refactored getDriver method to use GRID_URL environment variable for grid connection.

--- src/test/java/base/DriverFactory.java
+++ src/test/java/base/DriverFactory.java
@@ -11,6 +11,22 @@ public class DriverFactory {
     public static WebDriver getDriver(String browser) throws Exception {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setBrowserName(browser);
-        return new RemoteWebDriver(new URL("http://localhost:4444"), caps);
+        // return new RemoteWebDriver(new URL("http://localhost:4444"), caps); // local standalone grid
+
+        // select grid url from environment variable or use default
+        // String gridUrl = System.getenv().getOrDefault(
+        //         "GRID_URL",
+        //         "http://localhost:4444");
+        // return new RemoteWebDriver(new URL(gridUrl), caps);
+
+        // Use for both browsers (2 standalone grids)
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName(browser);
+
+        String gridUrl = System.getenv().getOrDefault(
+            "GRID_URL",
+            "http://localhost:4444"
+        );
+        return new RemoteWebDriver(new URL(gridUrl), caps);
     }
 }
