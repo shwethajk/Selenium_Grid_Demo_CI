537347148da03341afa1f3f28b5593337da8d097
Shwetha Kulal <90205229+shwethajk@users.noreply.github.com>
2026-02-01 23:53:02 +0530

    Simplify grid URL logic in DriverFactory
    Refactored grid URL retrieval to use a single environment variable.

--- src/test/java/base/DriverFactory.java
+++ src/test/java/base/DriverFactory.java
@@ -6,25 +6,39 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
 
+// public class DriverFactory {
+
+//     public static WebDriver getDriver(String browser) throws Exception {
+//         DesiredCapabilities caps = new DesiredCapabilities();
+//         caps.setBrowserName(browser);
+
+//         String gridUrl;
+
+//         if (browser.equalsIgnoreCase("chrome")) {
+//             gridUrl = System.getenv().getOrDefault(
+//                     "CHROME_GRID_URL",
+//                     "http://localhost:4444");
+//         } else if (browser.equalsIgnoreCase("firefox")) {
+//             gridUrl = System.getenv().getOrDefault(
+//                     "FIREFOX_GRID_URL",
+//                     "http://localhost:4445");
+//         } else {
+//             throw new RuntimeException("Unsupported browser: " + browser);
+//         }
+
+//         return new RemoteWebDriver(new URL(gridUrl), caps);
+//     }
+// }
+
 public class DriverFactory {
 
     public static WebDriver getDriver(String browser) throws Exception {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setBrowserName(browser);
 
-        String gridUrl;
-
-        if (browser.equalsIgnoreCase("chrome")) {
-            gridUrl = System.getenv().getOrDefault(
-                    "CHROME_GRID_URL",
-                    "http://localhost:4444");
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            gridUrl = System.getenv().getOrDefault(
-                    "FIREFOX_GRID_URL",
-                    "http://localhost:4445");
-        } else {
-            throw new RuntimeException("Unsupported browser: " + browser);
-        }
+        String gridUrl = System.getenv().getOrDefault(
+                "GRID_URL",
+                "http://localhost:4444");
 
         return new RemoteWebDriver(new URL(gridUrl), caps);
     }
